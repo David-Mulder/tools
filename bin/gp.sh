2ceb8359dd7a55b406c9b7cbe46b9add80455425
David Mulder <David-Mulder@users.noreply.github.com>
2015-11-18 21:27:33 +0100

    Update gp.sh

--- bin/gp.sh
+++ bin/gp.sh
@@ -22,7 +22,7 @@ branch=${3:-"master"} # default to master when branch isn't specified
 mkdir $repo
 pushd $repo >/dev/null
 git init
-git checkout -b --no-track gh-pages
+git checkout --orphan gh-pages
 git remote add origin git@github.com:$org/$repo.git
 echo "{
   \"name\": \"$2 documentation\"
